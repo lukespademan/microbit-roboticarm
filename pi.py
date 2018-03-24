b0a580a16d316ff6f1b5d35205723f51200af6d3
lukespademan <luke.spademan@gmail.com>
2018-03-24 16:10:29 +0000

    added comments and added elbow movement

--- pi.py
+++ pi.py
@@ -19,17 +19,18 @@ for m in r.moves:
 
 smoves["lo"] = "light-on"
 dir_to_move = {
-    "l0": "bc",
-    "r0": "bac",
-    "l1": "go",
-    "r1": "gc",
-    "d0": "su",
-    "u0": "sd",
-    "u1": "wd",
-    "d1": "wu",
+    "l0": "bc",  # base clockwise
+    "r0": "bac", # base anti clockwise
+    "l1": "go",  # grip open
+    "r1": "gc",  # grip close
+    "d0": "su",  # shoulder up
+    "u0": "sd",  # shoulder down
+    "u1": "wd",  # wrist down
+    "d1": "wu",  # wrist up
+    "d2": "eu",  # elbow up
+    "u2": "ed",  # elbow down
     "f": None
     }
-print(smoves)
 
 PORT = "/dev/ttyACM0"
 BAUD = 115200
